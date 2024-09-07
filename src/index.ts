692587d4e31f1f0d13c51f0241b081af59644783
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-09-07 14:19:43 +0200

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "./schedule";
-export * from "./persons";
-export * from "./events";
-export * from "./other";
+export { getActivityInfoFromSchedule, getGroupInfoByActivityId, prettyRoundFormat } from './schedule';
+export { getEventInfoFromWcif, getRoundInfoFromWcif, getEventIdFromRoundId, getCutoffByRoundId, getLimitByRoundId } from "./events";
+export { getNumberOfAttemptsForRound, getCompetitionDates } from "./other";
+export { getPersonFromWcif } from "./persons";
