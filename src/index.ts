c98a81b960d1faff846a33533e2252c0436343cb
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-09-07 14:37:46 +0200

    v0.1.7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { getActivityInfoFromSchedule, getGroupInfoByActivityId, prettyRoundFormat } from './schedule';
-export { getEventInfoFromWcif, getRoundInfoFromWcif, getEventIdFromRoundId, getCutoffByRoundId, getLimitByRoundId } from "./events";
-export { getNumberOfAttemptsForRound, getCompetitionDates } from "./other";
-export { getPersonFromWcif } from "./persons";
+export * from "./schedule";
+export * from "./persons";
+export * from "./events";
+export * from "./other";
